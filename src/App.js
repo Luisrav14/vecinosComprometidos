fead407ae4d324770d9a35d5c75f3f03ebe8f03e
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-07-30 11:15:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          vecinos Comprometidos  React.
+          vecinos Comprometidos organizado  React.
         </p>
         
       </header>
