5cee1e1e9b9dba46f85515610ae8ab5915410c15
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-08-05 12:50:09 -0500

    Update Index.jsx

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -78,6 +78,142 @@ export const Index = () => {
 
       
     </div>
+    <div class="row d-flex align-items-center justify-content-around mb-4">
+         <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+                <a href="https://www.cfe.mx/Pages/default.aspx" target="_blank"><img class="img-fluid" src="https://quintasresidencial.com/images/Mesa_de_trabajo_5.png" alt="servicio5" /></a>
+            </div>
+         </div>
+         
+         <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+               <a href="http://www.amd.gob.mx/amd/page.html"><img class="img-fluid" target="_blank" src="images/Mesa_de_trabajo_5_copia.png" alt="servicio6" /></a>
+            </div>
+         </div>
+  
+  
+        <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+               <a href="https://telmex.com" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_4_copia.png" alt="servicio7" /></a>
+            </div>
+         </div>
+         
+         
+         <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+               <a href="https://www.gasimperialdgo.com" target="_blank"><img class="img-fluid" src="images/iconos_Servicios_Gas.png" alt="servicio8" /></a>
+            </div>
+         </div>
+         
+         
+         <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+               <a href="https://www.megacable.com.mx" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_4_copia_6.png" alt="servicio12" /></a>
+            </div>
+         </div>
+    </div>
+
+  <div class="container pb-5">
+    <div class="col text-center pb-3">
+      <h5 id="subTitulo">Supermercados</h5>
+    </div>
+     <div class="row d-flex align-items-center justify-content-around mb-4">
+        
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+           <div class="card">
+        <a href="https://www.chedraui.com.mx" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_4.png" alt="servicio4" /></a>
+   
+       </div>
+       </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+        <a href="https://alsuper.com" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_1.png" alt="servicio1" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+            <div class="card">
+        <a href="https://www.samsclub.com" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_2.png" alt="servicio2" /></a>
+      </div>
+       </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex">
+          <div class="card">
+        <a href="https://www.cityclub.com.mx" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_3.png" alt="servicio3" /></a>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="container pb-5">
+    <div class="row pb-5">
+      <div class="col text-center pb-3"></div>
+    </div>
+    <div class="col text-center pb-3">
+      <h5 id="subTitulo">Oficios</h5>
+    </div>
+    <div class="row d-flex align-items-center justify-content-around mb-4">
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+            <div class="card">
+        <a href="https://api.whatsapp.com/send?phone=526182280145" target="_blank"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 7.png" alt="Deepers Lava Carros" /></a>
+      </div>
+          </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://api.whatsapp.com/send?phone=526181347748"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 8.png" alt="Cerrajeros VIP" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+                    <div class="card">
+        <a href="https://api.whatsapp.com/send?phone=526181496462"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 9.png" alt="Antonio Gallegos Plomero" /></a>
+      </div>
+       </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://api.whatsapp.com/send?phone=526181330663"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 10.png" alt="Manuel Plomero" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://api.whatsapp.com/send?phone=526182067292"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 11.png" alt="Alfonoso Jardinero" /></a>
+      </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container pb-5">
+    <div class="row pb-5">
+      <div class="col pb-1"></div>
+    </div>
+    <div class="col text-center pb-3">
+      <h5 id="subTitulo">Noticieros</h5>
+    </div>
+    <div class="row d-flex align-items-center justify-content-around mb-4">
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://www.canal12dgo.com" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_4_copia_3.png" alt="servicio9" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://canal10.com.mx/sitio/" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_4_copia_4.png" alt="servicio10" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="http://www.lobotv.com.mx" target="_blank"><img class="img-fluid" src="images/Mesa_de_trabajo_4_copia_5.png" alt="servicio11" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://envivodurango.com" target="_blank"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 14.png" alt="servicio12" /></a>
+      </div>
+      </div>
+      <div class="col-lg-2 col-md-6 mt-10 mb-30 d-flex justify-content-around">
+          <div class="card">
+        <a href="https://www.xhunestv.unes.edu.mx" target="_blank"><img class="img-fluid" src="images/Mesa de trabajo 4 copia 15.png" alt="servicio4" /></a>
+      </div>
+      </div>
+    </div>
+  </div>
     </div>
       </>
     );
