3b094485cc9abbf26db52fc3fdf1c8b2cb79c816
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-05 13:00:11 -0500

    Ruta inicio actualizada

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -2,21 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 
 import { LoginPage } from "../pages/LoginPage";
-import { HomePage } from "../pages/HomePage";
 import { Index } from "../pages/Index";
 
 export const Rutas = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route index element={<HomePage />} />
-        </Route>
+        <Route path="/" element={<Index />} />
         <Route path="/test" element={<App />} />
-
         <Route path="/login" element={<LoginPage />} />
-
-        <Route path="/index" element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
