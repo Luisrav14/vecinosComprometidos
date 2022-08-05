71b795438212dc08278c8a5723bd855d719f0721
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-08-05 12:10:23 -0500

    descripcion index

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,10 +1,10 @@
 export const Index = () => {
     return (
       <>
-       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light " style={{backgroundColor: '#40A4BC',}}>
-          <a className="navbar-brand" href="http://quintasresidencial.com"><img className='logo' src='https://vecinoscomprometidos.com/images/VC%20LOGO_white_vecinos.png' style={{width:150}}/></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light " style={{backgroundColor: '#40A4BC',}}>
+        <a className="navbar-brand" href="http://quintasresidencial.com"><img className='logo' src='https://vecinoscomprometidos.com/images/VC%20LOGO_white_vecinos.png' style={{width:150}}/></a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
             <div className="collapse navbar-collapse " id="navbarNavDropdown">
@@ -28,31 +28,56 @@ export const Index = () => {
             
       </nav>
       <div className="banner">
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    <img className='hola' src='https://quintasresidencial.com/images/1.png' style={{width:'100%'}}/>
-    </div>
-    <div class="carousel-item">
-    <img className='hola' src='https://quintasresidencial.com/images/2.png' style={{width:'100%'}}/>
-    </div>
-    <div class="carousel-item">
-    <img className='hola' src='https://quintasresidencial.com/images/3.png' style={{width:'100%'}}/>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img className='hola' src='https://quintasresidencial.com/images/1.png' style={{width:'100%'}}/>
+            </div>
+              <div class="carousel-item">
+                <img className='hola' src='https://quintasresidencial.com/images/2.png' style={{width:'100%'}}/>
+              </div>
+                <div class="carousel-item">
+                  <img className='hola' src='https://quintasresidencial.com/images/3.png' style={{width:'100%'}}/>
+                </div>
+          </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+      </div>
+      <div class="container pt-5 pb-5">
+    <div class="row pb-5">
+      <div class="col"></div>
+      <div class="col-12 pb-5 text-center">
+        <h1 id="titulo">UN LUGAR PARA <strong>DISFRUTAR EN FAMILIA</strong></h1>
+      
+        <h3 id="texto">Fraccionamiento privado, camellón central con área verde,</h3>
+        <h3 id="texto">juegos infantiles y áreas deportivas.</h3>
+        <h3 id="texto">Cuida tu casa, cuida tu familia.</h3>
+      </div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-      
+
+  <div class="container pb-5">
+    <div class="row pb-5">
+      <div class="col text-center pb-3">
+        <h1 id="titulo-2">RECURSOS PARA COLONOS</h1>
       </div>
+    </div>
+    <div class="row pb-5">
+      <div class="col pb-1"></div>
+    </div>
+    <div class="col text-center pb-3">
+      <h5 id="subTitulo">Servicios</h5>
+    </div>
 
       
+    </div>
     </div>
       </>
     );
