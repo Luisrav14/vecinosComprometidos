099abe162cf0d244f838652f8102a0f96ffcee1f
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-05 12:53:00 -0500

    Creación de login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { infoFraccionamiento } from "../global/infoFraccionamiento";
 import { useForm } from "../hooks/useForm";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 
 export const LoginForm = () => {
-  const { formState, onInputChange, email, password } = useForm({
-    email: "",
+  const { formState, onInputChange, correo, password } = useForm({
+    correo: "",
     password: "",
   });
 
+  const [inputType, SetinputType] = useState("password");
+
+  const showHiddePassword = () => (inputType == "password" ? SetinputType("text") : SetinputType("password"));
+
+  const handleSubmit = () => {};
+
   return (
-    <div className="d-lg-flex half">
-      <div className="container ">
+    <div className="d-lg-flex half mt-5">
+      <div className="container">
         <div className="row align-items-center justify-content-center">
           <div className="col-md-4 mb-5">
             <div className="text-center">
@@ -22,13 +30,18 @@ export const LoginForm = () => {
                 <label className="fw-normal mb-1" htmlFor="username">
                   Usuario
                 </label>
-                <input type="text" className="form-control" placeholder="Correo Electrónico" id="username" />
+                <input type="text" className="form-control" placeholder="Correo Electrónico" id="correo" name="correo" value={correo} onChange={onInputChange} />
               </div>
               <div className="form-group last my-3">
                 <label className="fw-normal mb-1" htmlFor="password">
                   Contraseña
                 </label>
-                <input type="password" className="form-control" placeholder="Contraseña" id="password" />
+                <div className="input-group mb-3">
+                  <input type={inputType} className="form-control" placeholder="Contraseña" id="password" name="password" value={password} onChange={onInputChange} />
+                  <button className="btn btn-outline-primary" type="button" onClick={showHiddePassword}>
+                    {inputType != "password" ? <BsFillEyeFill /> : <BsFillEyeSlashFill />}
+                  </button>
+                </div>
               </div>
               <input type="button" id="entrar" value="Entrar" className="btn btn-block form-control btn-primary form-inline" />
               <div className="d-flex pt-2 justify-content-center">
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
+import { FooterSm } from "../components/FooterSm";
 import { LoginForm } from "../components/LoginForm";
 
 export const LoginPage = () => {
   return (
     <>
       <LoginForm />
+      <FooterSm />
     </>
   );
 };
