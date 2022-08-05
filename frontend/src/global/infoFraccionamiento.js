13a0711afd83513b2fdc43c0f8ce150a84750c0a
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-05 18:40:06 -0500

    Backend para Heroku

--- frontend/src/global/infoFraccionamiento.js
+++
@@ -1,8 +0,0 @@
-import Logo from "../assets/images/logo.png";
-import LogoColor from "../assets/images/logo_color.png";
-
-export const infoFraccionamiento = {
-  nombre: "Vecinos Comprometidos",
-  logo: Logo,
-  logo_color: LogoColor,
-};
--- frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import "./assets/css/index.css";
-import { Rutas } from "./routes/router";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Rutas />);
