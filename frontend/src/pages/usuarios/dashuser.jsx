bfcdbe17ea493f1d652cc9b2bfd97fc4a8819cac
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-08-09 12:07:05 -0500

    Update dashuser.jsx

--- frontend/src/pages/usuarios/dashuser.jsx
+++ frontend/src/pages/usuarios/dashuser.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Navbar } from "../../components/admin/Navbar";
+import { Sidebar } from "../../components/admin/Sidebar";
+
+
 export const DashUser = () => {
-    return (
-      <>
-        <h1>DashUser</h1>
-      </>
-    );
-  };
-  
+  return (
+    <div className="wrapper">
+      <Sidebar />
+      <div className="main">
+        <Navbar />
+        <main className="content">
+          <div className="container-fluid p-0">
+            <h1 className="h3 mb-3">
+              <strong>Micrositio</strong> inicio
+            </h1>
+          <div className="col-12">
+          <img className="card-img-top" src="https://quintasresidencial.com/fraccionamiento/QuintasResidencial/assets/images/backgroudGeneral.jpeg" alt="" />
+          </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
