737fc9a90bf32a9c4f9805518920091e52e317f6
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-09 12:51:32 -0500

    Header y sidebar dashboard - Avance

--- frontend/src/components/admin/Navbar.jsx
+++ frontend/src/components/admin/Navbar.jsx
@@ -4,21 +4,19 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Administración /
-        </a>
+        {"<<<"}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link disabled">Resúmen de información</a>
+              <a className="nav-link disabled"> </a>
             </li>
           </ul>
           <form className="d-flex">
             <div class="dropdown mr-3">
-              <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              <button class="btn btn-outline-dark dropdown-toggle px-4" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 Usuario
               </button>
               <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
