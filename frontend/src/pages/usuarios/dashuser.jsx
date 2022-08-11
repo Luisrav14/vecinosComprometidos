bf546ebbac411b0b1d9d35fa3f09ebe4d2695070
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-11 14:42:54 -0500

    Sweet alert

--- frontend/src/pages/usuarios/dashuser.jsx
+++ frontend/src/pages/usuarios/dashuser.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import { Navbar } from "../../components/admin/Navbar";
 import { useEffect } from "react";
-import Swal from 'sweetalert2';
-import { SidebarUser } from '../../components/admin/SidebarUser';
-
-
+import Swal from "sweetalert2";
+import { SidebarUser } from "../../components/admin/SidebarUser";
 
 export const DashUser = () => {
-  useEffect(()=>{
+  useEffect(() => {
     mostrarAlerta();
-  },[]);
-  const mostrarAlerta=()=>{
+  }, []);
+  const mostrarAlerta = () => {
     Swal.fire({
-  
-      icon: 'success',
-      title: 'Inicio de sesion correcto',
+      icon: "success",
+      title: "Inicio de sesion correcto",
       showConfirmButton: false,
-      timer: 1500
+      timer: 1500,
     });
-
-  }
+  };
   return (
     <div className="wrapper">
       <SidebarUser />
@@ -27,19 +23,14 @@ export const DashUser = () => {
         <Navbar />
         <main className="content">
           <div className="container-fluid p-0">
-           
-
             <div className="row">
               <div className="col-12">
                 <div className="card">
                   <div className="card-header">
                     <h5 className="card-title mb-0"> </h5>
 
-
                     <h1 className="mb-3 fw-bold">Bienvenido</h1>
-                  <img className="card-img-top" src=" https://quintasresidencial.com/fraccionamiento/QuintasResidencial/assets/images/backgroudGeneral.jpeg"alt="" />
-                
-
+                    <img className="card-img-top" src=" https://quintasresidencial.com/fraccionamiento/QuintasResidencial/assets/images/backgroudGeneral.jpeg" alt="" />
                   </div>
 
                   <div className="card-body"></div>
