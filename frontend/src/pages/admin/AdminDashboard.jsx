677507b0a15c0dc15522bd6cf8fbcf0134a32a51
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-08-11 10:13:09 -0500

    Update AdminDashboard.jsx

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,7 +1,22 @@
 import { Navbar } from "../../components/admin/Navbar";
 import { Sidebar } from "../../components/admin/Sidebar";
-
+import { useEffect } from "react";
+import Swal from 'sweetalert2';
 export const AdminDashboard = ({ children }) => {
+  useEffect(()=>{
+    mostrarAlerta();
+  },[]);
+  const mostrarAlerta=()=>{
+    Swal.fire({
+  
+      icon: 'success',
+      title: 'Inicio de sesion correcto',
+      showConfirmButton: false,
+      timer: 1500
+    });
+
+  }
+
   return (
     <div className="wrapper">
       <Sidebar />
