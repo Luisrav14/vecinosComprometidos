40ea72465d1a754c7ea82dfd87b3145ccca2c79b
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-13 07:30:11 -0500

    Micrositio debug

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,9 +5,9 @@ export * from "./admin/AdminDashboard";
 export * from "./admin/BaseDatosGeneral";
 export * from "./admin/ResumenInformacion";
 
-export * from "./usuarios/Dashuser";
-export * from "./usuarios/Efectivo";
-export * from "./usuarios/PagosPendientes";
-export * from "./usuarios/SaldoFavor";
-export * from "./usuarios/Suscripcion";
-export * from "./usuarios/Transferencia";
+export * from "./usuarios/Dashuser.jsx";
+export * from "./usuarios/Efectivo.jsx";
+export * from "./usuarios/PagosPendientes.jsx";
+export * from "./usuarios/SaldoFavor.jsx";
+export * from "./usuarios/Suscripcion.jsx";
+export * from "./usuarios/Transferencia.jsx";
