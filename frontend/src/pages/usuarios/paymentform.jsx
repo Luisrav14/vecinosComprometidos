1822c296d64d95dbf5368497b8f87dfbbf1a9e53
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-08-15 12:51:41 -0500

    Update paymentform.jsx

--- frontend/src/pages/usuarios/paymentform.jsx
+++ frontend/src/pages/usuarios/paymentform.jsx
@@ -1,52 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 
 
 
-const PaymentForms =() =>{
-    return(
+const PaymentForms = () => {
+    const [state, setState] = useState({
+        number: "",
+        name: "",
+        expiry: "",
+        cvc: "",
+        focus: ""
+    })
+    const handleInputChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+            [e.target.number]: e.target.value,
+            [e.target.expiry]: e.target.value,
+            [e.target.cvc]: e.target.value
+        })
+    }
+
+    const handleFocusChange = (e) => {
+        setState({
+            ...state,
+            focus: e.target.name
+        })
+    }
+    return (
         <div className='card-body'>
-    <Cards
-        number={46456789123}
-        name={"Ernesto"}
-        expiry={1020}
-        cvc={"123"}
-        focus="name"
-    />
-    <form>
-        <div className='form-group'>
-            <label htmlFor="name">Nombre</label>
-            <input className='form-control' placeholder='Nombre' type="text" name="" id="" />
-        </div>
+            <Cards
+                number={state.number}
+                name={state.name}
+                expiry={state.expiry}
+                cvc={state.cvc}
+                focused={state.focus} />
+            <form>
+                <div className='form-group'>
+                    <label htmlFor="name">Nombre</label>
+                    <input className='form-control' placeholder='Nombre' type="text" name="name" id="name" onChange={handleInputChange} onFocus={handleFocusChange} />
+                </div>
 
-        <div className='form-group'>
-            <label htmlFor="name">Numero de la tarjeta</label>
-            <input className='form-control' type="number" placeholder='•••• •••• •••• ••••' name="" id="" />
-        </div>
-        <div className='row'>
-        <div class="col">
-        <label htmlFor="name">Mes de expiración</label>
-        <select class="form-select" aria-label="Default select example">
-  <option selected>Selecciona un mes</option>
-  <option value="1">Enero</option>
-  <option value="2">Febrero</option>
-  <option value="3">Marzo</option>
-</select>
-    </div>
-    <div class="col">
-    <label htmlFor="name">Dia de expiración</label>
-            <input className='form-control' type="number" placeholder='••' name="" id="" />
-    </div>
-    <div class="col">
-    <label htmlFor="name"> CVV</label>
-            <input className='form-control' type="number" placeholder='•••' name="" id="" />
-    </div>
-        </div>
-    </form>
+                <div className='form-group'>
+                    <label htmlFor="name">Numero de la tarjeta</label>
+                    <input className='form-control' type="number" placeholder='•••• •••• •••• ••••' name="number" id="number" onChange={handleInputChange} onFocus={handleFocusChange} maxLength="16" />
+                </div>
+                <div className='row'>
+                    <div class="col">
+                        <div className='form-group'>
+                            <label htmlFor="name">Fecha de expiracion </label>
+                            <input className='form-control' placeholder='Fecha de expiracion' type="number" name="expiry" id="expiry" onChange={handleInputChange} onFocus={handleFocusChange}  maxLength="4" />
+                        </div>
 
-    </div>
-  );
+                    </div>
+
+                    <div class="col">
+                        <label htmlFor="name"> CVV</label> <br></br>
+                        <input className='form-control' type="text" placeholder='•••' name="cvc" id="cvc" onChange={handleInputChange} onFocus={handleFocusChange}  maxLength="4"/>
+                    </div>
+                </div>
+                <button type="button" className=" btn btn-success btn-block btn-lg">Pagar</button>
+            </form>
+
+        </div>
+    );
 };
 
 export default PaymentForms;
