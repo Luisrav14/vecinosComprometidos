15b99d8826963a1e2bd3fe0baa70a0811a785345
Luis Valenzuela <luisrav14@hotmail.com>
2022-08-31 13:06:58 -0500

    ExportDT
    Datatable

--- backend/src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { loginController } from "../controllers/loginController";
-
-const router = Router();
-
-router.get("/test", loginController.test);
-router.post("/login", loginController.validation);
-
-export default router;
