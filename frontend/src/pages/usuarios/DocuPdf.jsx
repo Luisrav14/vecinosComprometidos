2871d99a4877fcd83f6df4fa507e13ca654f15af
Ernesto Rocha <79719276+ErnestoAmayaRocha@users.noreply.github.com>
2022-09-01 11:36:02 -0500

    fullcalendar

---
+++ frontend/src/pages/usuarios/DocuPdf.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+
+
+
+export default class DocuPdf extends React.Component {
+    render(){
+        return(
+           
+    <FullCalendar/>
+        )
+    }
+
+
+}
